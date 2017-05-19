9421cf806cef5b7df6ce4c7afa70bf72f8ae0108
alexitaylor <alexi@bluesunrise.com>
2017-05-18 19:25:38 -0700

    Cleaning up code

--- client/src/components/AddItem.jsx
+++ client/src/components/AddItem.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import AddItemForm from './AddItemForm.jsx';
 import AddItemDialog from './AddItemDialog.jsx';
 import BarcodeSearchDialog from './BarcodeSearchDialog.jsx';
 
-class AddItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-   return (
-        <div>
-        <div className="add-item">
-          <BarcodeSearchDialog  houseId={this.props.houseId} submitItem={this.props.submitItem} />
-        </div>
-        <div className="add-item">
-          <AddItemDialog  houseId={this.props.houseId} submitItem={this.props.submitItem} />
-        </div>
-        </div>
-      );
-  }
-}
+const AddItem = (props) => {
+  return (
+    <div>
+      <div className="add-item">
+        <BarcodeSearchDialog houseId={props.houseId} submitItem={props.submitItem} />
+      </div>
+      <div className="add-item">
+        <AddItemDialog houseId={props.houseId} submitItem={props.submitItem} />
+      </div>
+    </div>
+  );
+};
+
 
 export default AddItem;
