ea56bc7dfddffd2c9565b8871ea692a1569f9051
Jason Yu <jasonyu@Jasons-MacBook-Pro.local>
2017-05-21 21:12:03 -0700

    fixed enter handler

--- client/src/components/AddItemSelect.jsx
+++ client/src/components/AddItemSelect.jsx
@@ -56,6 +56,7 @@ class AddItemSelect extends Component {
       showSelection: true,
     };
     this.handleSelection = this.handleSelection.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   postItem(obj) {
@@ -99,6 +100,16 @@ class AddItemSelect extends Component {
     }
   }
 
+<<<<<<< HEAD
+=======
+  handleKeyDown(event) {
+    if (event.key === 'Enter'){
+      event.preventDefault();
+      this.handleSubmit();
+    }
+  }
+
+>>>>>>> fixed enter handler
   render() {
     if (this.state.showSelection) {
       return (
@@ -122,12 +133,17 @@ class AddItemSelect extends Component {
     } else {
       return (
         <div style={styles.selected}>
+<<<<<<< HEAD
           <img src={this.state.image} alt="food" />
           <TextField
             floatingLabelText="Item Price (optional)"
             value={this.state.price}
             onChange={this.savePrice.bind(this)}
           />
+=======
+          <img src={this.state.image} />
+          <TextField floatingLabelText="Item Price" value={this.state.price} onChange={this.savePrice.bind(this)} onKeyDown={this.handleKeyDown}/>
+>>>>>>> fixed enter handler
           <RaisedButton primary label="Submit" onClick={() => this.handleSubmit()} />
         </div>
       );
